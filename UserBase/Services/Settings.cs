7fe6b3aa3aba49fc4533391a77c67ca0af769b47
aismaniotto <aismaniotto@gmail.com>
2020-11-01 17:00:42 -0300

    feat(settings): create settings static class

---
+++ UserBase/Services/Settings.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xamarin.Essentials;
+
+namespace UserBase.Services
+{
+    public static class Settings
+    {
+        private const string IdUrlBase = "url_base";
+        private static readonly string UrlBaseDefault = "https://run.mocky.io/v3";
+
+        private const string IdAuthToken= "auth_token";
+        private static readonly string AuthTokenDefault = "";
+
+        private const string IdUser = "user_email";
+        private static readonly string UserDefault = "";
+
+        public static string UrlBase
+        {
+            get
+            {
+                return Preferences.Get(IdUrlBase, UrlBaseDefault);
+            }
+            set
+            {
+                Preferences.Set(IdUrlBase, value);
+            }
+        }
+
+        public static string AuthToken
+        {
+            get
+            {
+                return Preferences.Get(IdAuthToken, AuthTokenDefault);
+            }
+            set
+            {
+                Preferences.Set(IdAuthToken, value);
+            }
+        }
+
+        public static string User
+        {
+            get
+            {
+                return Preferences.Get(IdUser, UserDefault);
+            }
+            set
+            {
+                Preferences.Set(IdUser, value);
+            }
+        }
+    }
+}
